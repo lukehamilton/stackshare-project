d21cb66bf6c6bfdffc05e748c6eccd689fb8d023
Luke Hamilton <luke@cloud.com>
2016-11-21 16:18:47 -0500

    Added React routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import reducer from './reducers'
+import App from './Components/App';
+import Tags from './Components/Tags';
+import Tools from './Components/Tools';
 
-render(<App name='World'/>, document.getElementById('root'));
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Tags}/>
+        <Route path="tags/:tagID/tools" component={Tools}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
